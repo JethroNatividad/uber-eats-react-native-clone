ebb22c866243da733a0b2bb4b0c416605bd2cf7c
JethroSama <kel73896@gmail.com>
2021-10-04 00:15:36 +0800

    add search components & install dependencies

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { View, Text, SafeAreaView, StatusBar } from 'react-native'
 import HeaderTabs from '../components/HeaderTabs'
+import Search from '../components/Search'
 
 export default function Home() {
     return (
         <SafeAreaView style={{ paddingTop: StatusBar.currentHeight, backgroundColor: '#eee', flex: 1 }} >
             <View style={{ backgroundColor: '#fff', padding: 15 }}>
                 <HeaderTabs />
+                <Search />
             </View>
         </SafeAreaView >
     )
